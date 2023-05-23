7f43a08ac99536b016e745a65cd2a25835ed8eba
Aditya Pratama <aditypraa@gmail.com>
2023-05-23 21:37:42 +0700

    Penambahan Tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nik')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['operator', 'keuangan', 'marketing'])->default('operator');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
